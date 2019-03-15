53b6c2a0af7f7e8e2d684082150c368d933d388d
kozinho <kozinho96@Interia.pl>
2019-03-15 15:30:13 +0100

    add restructure

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 let nav = '<nav> <a href="index.html">Home</a>' +
-          '<a href="#">Blog</a>' +
-          '<a href="#">Quiz</a>' +
-          '<a href="contact.html">Kontakt</a></nav>';
+    '<a href="#">Blog</a>' +
+    '<a href="#">Quiz</a>' +
+    '<a href="contact.html">Kontakt</a></nav>';
 
 let navVariable = document.querySelector(".navbar");
 navVariable.innerHTML = nav;
